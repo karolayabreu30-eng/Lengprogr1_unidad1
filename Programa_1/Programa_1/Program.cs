012b5ce3d31545fc7a858b4c06e862de1b86f576
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-07 21:08:57 -0400

    Delete Programa_1 directory

--- Programa_1/Programa_1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("---suma---");
-Console.WriteLine("Primer numero:");
-int a = int.Parse(s: Console.ReadLine());
-Console.WriteLine("Segundo Numero: ");
-int b=int.Parse(Console.ReadLine());
-
-int Suma = a + b;
-Console.WriteLine("La suma es: " + Suma);
